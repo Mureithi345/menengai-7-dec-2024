91d4a710f96f0c4d833f4b0c22b148a3df4b4206
Repent Office <repentoffice@Repents-iMac.local>
2024-12-30 00:51:24 +0300

    saved HTML

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Array of image paths
 const images = [
-    "Slide-1.jpg", "Slide-2.jpg",
+    "Slide-1.jpg", "Slide-2.jpg","Slide-3.jpg","Slide-4.jpg","Slide-5.jpg", "Slide-6.jpg", "Slide-7.jpg", "Slide-8.jpg"
     
 ];
 
