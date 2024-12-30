79459230fab27f815ae5ab69f4d0b454d75e2d0a
Repent Office <repentoffice@Repents-iMac.local>
2024-12-30 19:31:06 +0300

    updated slide 1 & 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Array of image paths
 const images = [
-    "Slide-1.jpg", "Slide-2.jpg","Slide-3.jpg","Slide-4.jpg","Slide-5.jpg", "Slide-6.jpg", "Slide-7.jpg", "Slide-8.jpg", "Slide-9.jpg", "Slide-10.jpg", "Slide-11.jpg", "Slide-12.jpg", "Slide-13.jpg", "Slide-14.jpg", "Slide-15.jpg", "Slide-16.jpg", "Slide-17.jpg", "Slide-18.jpg", "Slide-19.jpg", "Slide-20.jpg", "Slide-21.jpg", "Slide-22.jpg",  "Slide-23.jpg", "Slide-24.jpg"      
+    "Slide-1.jpg", "Slide-2.jpg","Slide-3.jpg", "Slide-4.jpg", "Slide-5.jpg", "Slide-6.jpg", "Slide-7.jpg", "Slide-8.jpg", "Slide-9.jpg", "Slide-10.jpg", "Slide-11.jpg", "Slide-12.jpg", "Slide-13.jpg", "Slide-14.jpg", "Slide-15.jpg", "Slide-16.jpg", "Slide-17.jpg", "Slide-18.jpg", "Slide-19.jpg", "Slide-20.jpg", "Slide-21.jpg", "Slide-22.jpg",  "Slide-23.jpg", "Slide-24.jpg"      
     
 ];
 
